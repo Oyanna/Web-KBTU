0560d860d450f0d7273f9eb751d8d5c9a5ddc5d7
Oyanna <moonlightreprise@gmail.com>
2021-04-08 22:26:49 +0600

    w10

--- w4/MyFirstProject/src/app/products.ts
+++ w4/MyFirstProject/src/app/products.ts
@@ -1,40 +1,133 @@
 export const products = [
   {
-    name: 'Мужской костюм для мужчин смокинг, костюм для свадьбы на заказ, 2019',
-    price: 8808.39,
+    name: 'Увлажняющий крем CeraVe | Крем для тела с гиалуроновой кислотой и керамидами | 19 унций',
+    price: 16.08,
     // tslint:disable-next-line:max-line-length
-    description: 'Мужской костюм темно-синего цвета на заказ, костюм на заказ, светло-темно-синие свадебные костюмы на заказ для мужчин, облегающие смокинги жениха для мужчин (пиджак + брюки + галстук + Карманный квадрат) 13042402',
+    description: 'Керамиды естественным образом содержатся в коже и составляют 50% липидов кожного барьера. Все продукты CeraVe содержат три основных керамида (1, 3, 6-II), которые помогают восстановить и поддерживать естественный барьер кожи.',
     rating: 4.9,
-    image1: 'https://ae01.alicdn.com/kf/HTB1ex4OKf9TBuNjy1zbq6xpepXaF/-.jpg_50x50.jpg',
-    image2: 'https://ae01.alicdn.com/kf/HTB1HGQ0JVGWBuNjy0Fbq6z4sXXa0/-.jpg_50x50.jpg',
-    image3: 'https://ae01.alicdn.com/kf/HTB1OcxzKeuSBuNjSsplq6ze8pXat/-.jpg_50x50.jpg',
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/61S7BrCBj7L._SL1000_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/61XWTVUahpL._SL1000_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/61ZakQhf6-L._SL1000_.jpg',
     // tslint:disable-next-line:max-line-length
-    link: 'https://aliexpress.ru/item/2046453649.html?spm=a2g0o.productlist.0.0.3c142186qtu3lw&s=p&ad_pvid=202003290839262247633025897680000090700_1&algo_pvid=017c88c4-5680-4840-8fdd-61f9dec32857&algo_expid=017c88c4-5680-4840-8fdd-61f9dec32857-0&btsid=0ab6fb8315854963663168653ef75b&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+    link: 'https://www.amazon.com/CeraVe-Moisturizing-Cream-Daily-Moisturizer/dp/B00TTD9BRC/ref=lp_16225006011_1_3'
   },
   {
     // tslint:disable-next-line:max-line-length
-    name: 'Мужской костюм Shenrun',
-    price: 4528.26,
+    name: 'Касторовое масло (2 унции), сертифицированное USDA органическое',
+    price: 9.99,
     // tslint:disable-next-line:max-line-length
-    description: 'Мужской костюм Shenrun, облегающий двубортный костюм темно-синего и черного цвета с отворотом, для свадьбы, вечеринки, выпускного бала',
+    description: '100% чистый продукт холодного отжима, без гексана от Кейт Блан. Стимулируют рост ресниц, бровей, волос. Сыворотка для роста ресниц. Лечение бровей. БЕСПЛАТНЫЙ стартовый набор для туши',
     rating: 4.8,
-    image1: 'https://ae01.alicdn.com/kf/H3b38604186b44e2db89301af0c4bffe6E/Shenrun.jpg_50x50.jpg',
-    image2: 'https://ae01.alicdn.com/kf/Hfab2b346a6834b10abea3dc74615f8c5z/Shenrun.jpg_50x50.jpg',
-    image3: 'https://ae01.alicdn.com/kf/H4d94cd83ff594d8188d8c8dd4cb76e5ec/Shenrun.jpg_50x50.jpg',
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/615AiiiHAUL._SL1100_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71Yo6DxPm0L._SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/6119BXuMB5L._SL1200_.jpg',
     // tslint:disable-next-line:max-line-length
-    link: 'https://aliexpress.ru/item/33036452414.html?spm=a2g0o.productlist.0.0.3c142186qtu3lw&s=p&ad_pvid=202003290839262247633025897680000090700_5&algo_pvid=017c88c4-5680-4840-8fdd-61f9dec32857&algo_expid=017c88c4-5680-4840-8fdd-61f9dec32857-4&btsid=0ab6fb8315854963663168653ef75b&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+    link: 'https://www.amazon.com/Certified-Kate-Stimulate-Eyelashes-Treatment/dp/B01NALN8Q9/ref=sr_1_4?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=NE1YZ166TCCQ59HXGC4J&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-4'
   },
   {
     // tslint:disable-next-line:max-line-length
-    name: 'Мужской костюм Womail',
-    price: 1705.26,
+    name: 'Щетка-шампунь для массажа кожи головы волос',
+    price: 5.26,
     // tslint:disable-next-line:max-line-length
-    description: 'Мужской костюм, модные комплекты, тонкий костюм из 3 предметов, деловой костюм для свадебной вечеринки, хлопковая куртка, жилет и штаны, высококачественный однотонный мужской костюм, Новинка',
-    rating: 4.0,
-    image1: 'https://ae01.alicdn.com/kf/H91494fc17a6f461292bfea043df9af6b5/3.jpg_50x50.jpg',
-    image2: 'https://ae01.alicdn.com/kf/H4161b6d575244f619a36c9ab3abb4a32h/3.jpg_50x50.jpg',
-    image3: 'https://ae01.alicdn.com/kf/H8bea76d390174b02b2afe1af6a202a03y/3.jpg_50x50.jpg',
+    description: 'Щетка для шампуня с идеальной ручкой для удобного ношения в руке (ручная работа без батареи и водонепроницаемость)',
+    rating: 4.8,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/51EbgIgofzL._SL1036_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/51t5xRPabJL._SL1036_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/612dSY9ayVL._SL1036_.jpg',
     // tslint:disable-next-line:max-line-length
-    link: 'https://aliexpress.ru/item/4000193986081.html?spm=a2g0o.productlist.0.0.3c142186qtu3lw&algo_pvid=017c88c4-5680-4840-8fdd-61f9dec32857&algo_expid=017c88c4-5680-4840-8fdd-61f9dec32857-9&btsid=0ab6fb8315854963663168653ef75b&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_'
+    link: 'https://www.amazon.com/Scalp-Massager-Shampoo-Brush-MAXSOFT/dp/B074ZDXFL6/ref=sr_1_7?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=NE1YZ166TCCQ59HXGC4J&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-7'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'REVLON One-Step Фен и щетка для волос с горячим воздухом, черный, упаковка может варьироваться',
+    price: 45.26,
+    // tslint:disable-next-line:max-line-length
+    description: 'Обеспечивает великолепный объем и сияющий блеск за один прием',
+    rating: 4.4,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/71aXzv34N%2BL._SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/81qE4AMwzxL._SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/81FGuBYd2FL._SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Revlon-One-Step-Dryer-Volumizer-Brush/dp/B01LSUQSB0/ref=sr_1_9?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=NE1YZ166TCCQ59HXGC4J&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-9'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Парикмахерские ножницы для стрижки волос Equinox Professional Razor Edge Series',
+    price: 25.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'Ножницы для салонов из нержавеющей стали apanese - Общая длина 6,5 дюйма - Натяжной винт точной регулировки - Ножницы премиум-класса для стрижки волос',
+    rating: 4.3,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/61sKdG3rSZL._SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/61xmDE-2zdL._SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/61doNvLUs1L._SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Equinox-Professional-Shears-Razor-Edge/dp/B00KVYBY8M/ref=sr_1_16?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=NE1YZ166TCCQ59HXGC4J&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-16'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Увлажняющая маска для волос и кондиционер с маслом арганы',
+    price: 12.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'Профессиональный салонный уход, который мгновенно преобразует текстуру ваших волос, делая их мягкими, шелковистыми и легкими в уходе',
+    rating: 4.1,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/71HB8ShCY5L._SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/81VpKb%2BTVAL._SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/7102VpOcI8L._SL1421_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Hydrating-Argan-Conditioner-Arvazallia-Damaged/dp/B00I32AN4K/ref=sr_1_18?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=NE1YZ166TCCQ59HXGC4J&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-18'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Контролирует тетя Джеки 15 унций - Увлажняющий и смягчающий кондиционер "Anti-Poof"',
+    price: 6.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'ломкие волосы с натуральными экстрактами и протеинами, питают и глубоко вьются, трудно поддаются уходу',
+    rating: 4.5,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/7167r9YjfYL._SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/71qZpVbhCiL._SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/61wFJiUonwL._SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Aunt-Jackies-control-15oz-Moisturizing/dp/B008R3GVN2/ref=pd_vtp_4?pd_rd_w=D9LZZ&pf_rd_p=55cbb45e-2534-4809-9135-12f41eecb852&pf_rd_r=DEMTCYMVA0FCWMWMVZ4Y&pd_rd_r=08b490fa-50d2-461e-9d06-6c56e74f329c&pd_rd_wg=jwML8&pd_rd_i=B008R3GVN2&psc=1'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Масло арганы, белый, зеленый чай, масло ши, 13 жидких унций',
+    price: 12.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'МЯГКИЕ, БЛЕСКИ И УПРАВЛЯЕМЫЕ ВОЛОСЫ: Наш увлажняющий кондиционер с экстрактом зеленого чая содержит масло арганы, которое проникает в кожу головы, питая корни волос и способствуя росту волос.',
+    rating: 4.7,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/81MQ692AVML._SL1200_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/417g43mTbrL.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/51js5-IS4ML.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/tgin-Green-Leave-Conditioner-Natural/dp/B00V73BZSU/ref=pd_vtp_2?pd_rd_w=VVipo&pf_rd_p=55cbb45e-2534-4809-9135-12f41eecb852&pf_rd_r=KDN9ER25RG768QB3FAVN&pd_rd_r=e93e850e-bb70-4151-b626-662086af7c8d&pd_rd_wg=aZmc2&pd_rd_i=B00V73BZSU&psc=1'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Маска для волос с маслом ши и несмываемый кондиционер для глубокого питания локонов и восстановления секущихся кончиков',
+    price: 6.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'Маска для волос с маслом ши и несмываемый уход, Идеально подходит для волнистых и плотных локонов и среднего и сухого уровней влажности',
+    rating: 4.9,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/61sJ0y2U8rL._SL1000_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/614A5FnASML._SL1000_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/61UcW-iZNuL._SL1000_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Maui-Moisture-Hydrate-Butter-Ounce/dp/B01MXCO41W/ref=pd_vtp_14?pd_rd_w=VVipo&pf_rd_p=55cbb45e-2534-4809-9135-12f41eecb852&pf_rd_r=KDN9ER25RG768QB3FAVN&pd_rd_r=e93e850e-bb70-4151-b626-662086af7c8d&pd_rd_wg=aZmc2&pd_rd_i=B01MXCO41W&psc=1'
+  },
+  {
+    // tslint:disable-next-line:max-line-length
+    name: 'Витаминная кератиновая маска для волос Deep Conditioner',
+    price: 19.45,
+    // tslint:disable-next-line:max-line-length
+    description: 'Биотиновый протеин с касторовым маслом Восстановление для сухих поврежденных и окрашенных волос - Кондиционер для вьющихся или прямых тонких волос',
+    rating: 4.8,
+    image1: 'https://images-na.ssl-images-amazon.com/images/I/71U%2B1Y9ONLL._SL1500_.jpg',
+    image2: 'https://images-na.ssl-images-amazon.com/images/I/8179cLHVt%2BL._SL1500_.jpg',
+    image3: 'https://images-na.ssl-images-amazon.com/images/I/81vXR2UaUWL._SL1500_.jpg',
+    // tslint:disable-next-line:max-line-length
+    link: 'https://www.amazon.com/Vitamins-Keratin-Hair-Mask-Conditioner/dp/B014FVQSAA/ref=pd_vtp_7?pd_rd_w=6Priw&pf_rd_p=55cbb45e-2534-4809-9135-12f41eecb852&pf_rd_r=8TNEC4D19QY7HAW7PFRD&pd_rd_r=9778ba8e-146d-4263-9f4a-592174029e68&pd_rd_wg=Rk5Ty&pd_rd_i=B014FVQSAA&psc=1'
   }
+
+
 ];
